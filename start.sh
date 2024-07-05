af832dd999d56813b6575445d31e0546321c82e0
José Vicente <j.vicentem@alumnos.urjc.es>
2024-07-05 18:38:09 +0200

    Fix deployment. Add one more fav pl

--- start.sh
+++ start.sh
@@ -2,16 +2,19 @@
 
 cd /Users/jose/spotify-music-curator
 
-pyenv activate music_curator
-
 cd ./spoti_curator
-
 eval $(cat .env | sed 's/^/export /')
-
 cd ..
+
+export PYTHONPATH=$PYTHONPATH:$PWD
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+pyenv activate music_curator
 
 python ./spoti_curator/app.py
 
-echo $SPECIAL_ENV | sudo -S -k pmset repeat wakeorpoweron F 16:30:00
+#echo $SPECIAL_ENV | sudo -S -k pmset repeat wakeorpoweron F 16:30:00
 
-echo $SPECIAL_ENV | sudo -S -k pmset sleepnow
+#echo $SPECIAL_ENV | sudo -S -k pmset sleepnow
